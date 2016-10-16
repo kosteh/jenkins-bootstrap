2ea347f67f0469c517042e849d40a43c2fa8e8e6
kosteh <kosteh@users.noreply.github.com>
2016-10-16 22:38:36 +0200

    Create 500-configure-email.groovy

---
+++ docker-image/ref/init.groovy.d/500-configure-email.groovy
@@ -0,0 +1,15 @@
+import jenkins.model.*
+
+def inst = Jenkins.getInstance()
+def desc = inst.getDescriptor("hudson.tasks.Mailer")
+
+// Define mail descriptor
+desc.setSmtpAuth(System.getProperty("smtp.user"), System.getProperty("smtp.password"))
+desc.setReplyToAddress(System.getProperty("smtp.replyToAddress"))
+desc.setSmtpHost(System.getProperty("smtp.host"))
+desc.setUseSsl(Boolean.valueOf(System.getProperty("smtp.useSsl")))
+desc.setSmtpPort(System.getProperty("smtp.port"))
+desc.setCharset(System.getProperty("smtp.charSet"))
+
+// Persist it
+desc.save()
