2166558dd67d15a2d77fa21919b1a43cda9f278a
kosteh <kosteh@users.noreply.github.com>
2016-10-17 18:57:03 +0200

    Update 501-configure-email.groovy

--- docker-image/ref/init.groovy.d/501-configure-email.groovy
+++ docker-image/ref/init.groovy.d/501-configure-email.groovy
@@ -1,46 +1,39 @@
 import jenkins.model.*
 
-def inst     = Jenkins.getInstance()
-def desc     = inst.getDescriptor("hudson.tasks.Mailer")
-def String password = null
+def mailConfigurationUsername = "outbound-mail-configuration"
+def credentials = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
+                       com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials.class,
+                       jenkins.model.Jenkins.instance )
 
-def getPassword = { username ->
-    def creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
-        com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials.class,
-        jenkins.model.Jenkins.instance
-    )
+def mailCredentials = credentials.findResult { it.username == mailConfigurationUsername ? it : null }
 
-    def c = creds.findResult { it.username == username ? it : null }
+if ( ! mailCredentials ) {
+    println "Jenkins credential NOT found for ${mailConfigurationUsername}"
+} else {
+    println "Jenkins credential ${mailCredentials.id} found for username ${mailCredentials.username}"
+    
+    // Password contains:
+    // <host>:<port>:<ssl?>:<account>:<password>:<replytoaddress>:<charset>
+    // E.g: mail.hdml.nl:25:true:hans@hdml.nl:<secret>:noreply@hdml.nl:UTF-8
+    def String password = mailCredentials.password
+    def String[] omc    = password.split(":")
+  
+    println "Mail host    = ${omc[0]}"
+    println "Mail port    = ${omc[1]}"
+    println "SSL ?        = ${omc[2]}"
+    println "Mail account = ${omc[3]}"
+    println "Mail passwd  = <secret>"
+    println "Mail replyTo = ${omc[5]}"
+    println "Mail charset = ${omc[6]}"
 
-    if ( c ) {
-        println "Found credential ${c.id} for username ${c.username} ${c.password} "
-        password = c.password
-    } else {
-        println "Credential NOT found for ${username}"
-    }
+    def mailDesc = jenkins.model.Jenkins.instance.getDescriptor("hudson.tasks.Mailer")
+    mailDesc.setSmtpHost(omc[0])
+    mailDesc.setSmtpPort(omc[1])
+    mailDesc.setUseSsl(Boolean.valueOf(omc[2]))
+    mailDesc.setSmtpAuth(omc[3], omc[4])
+    mailDesc.setReplyToAddress(omc[5])
+    mailDesc.setCharset(omc[6])
+
+    // Persist it
+    mailDesc.save()
 }
-
-getPassword ("outbound-mail-configuration")
-
-// Password contains:
-// <host>:<port>:<ssl?>:<account>:<password>:<replytoaddress>:<charset>
-// E.g: mail.hdml.nl:25:true:hans@hdml.nl:Diklba00@#$:noreply@hdml.nl:UTF-8
-def String[] omc = password.split(":")
-println "Mail host    = ${omc[0]}"
-println "Mail port    = ${omc[1]}"
-println "SSL ?        = ${omc[2]}"
-println "Mail account = ${omc[3]}"
-println "Mail passwd  = <secret>"
-println "Mail replyTo = ${omc[5]}"
-println "Mail charset = ${omc[6]}"
-
-// Define mail descriptor
-desc.setSmtpHost(omc[0])
-desc.setSmtpPort(omc[1])
-desc.setUseSsl(Boolean.valueOf(omc[2]))
-desc.setSmtpAuth(omc[3], omc[4])
-desc.setReplyToAddress(omc[5])
-desc.setCharset(omc[6])
-
-// Persist it
-desc.save()
