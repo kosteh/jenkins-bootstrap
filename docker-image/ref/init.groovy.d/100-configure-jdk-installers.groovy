38d4990b1a374e3a3f6854afdd9f507999e56c3a
kosteh <kosteh@users.noreply.github.com>
2016-10-17 22:21:50 +0200

    Update 100-configure-jdk-installers.groovy

--- docker-image/ref/init.groovy.d/100-configure-jdk-installers.groovy
+++ docker-image/ref/init.groovy.d/100-configure-jdk-installers.groovy
@@ -1,3 +1,13 @@
+/*
+
+Purpose:
+Configures a number of JDK auto-installers.
+
+Usage:
+Define one or more versions in the "versions" map.
+
+*/
+
 import jenkins.model.*
 import hudson.model.*
 import hudson.tools.*
@@ -5,6 +15,7 @@ import hudson.tools.*
 def inst = Jenkins.getInstance()
 def desc = inst.getDescriptor("hudson.model.JDK")
 
+// Define and configure a number of JDK installations
 def versions = [
   "JDK Latest" : "jdk-8u102-oth-JPR",
   "JDK 8u102"  : "jdk-8u102-oth-JPR"
@@ -19,5 +30,6 @@ for (v in versions) {
   installations.push(installation)
 }
 
+// Persist the JDK configuration
 desc.setInstallations(installations.toArray(new JDK[0]))
 desc.save() 
