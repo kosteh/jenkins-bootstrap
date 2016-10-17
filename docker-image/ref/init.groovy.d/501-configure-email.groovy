f1247627e4c289aa948383f3080708c69e599c25
kosteh <kosteh@users.noreply.github.com>
2016-10-17 20:22:58 +0200

    Update 501-configure-email.groovy

--- docker-image/ref/init.groovy.d/501-configure-email.groovy
+++ docker-image/ref/init.groovy.d/501-configure-email.groovy
@@ -1,29 +1,60 @@
+/*
+
+Purpose:
+
+This scripts configures the Jenkins e-mail configuration, that can be specified manually through the
+configuration page "Jenkins->Manage Jenkins->Configure System".
+
+The configuration parameters are provided through environment variables mainly. The password is provided 
+by a Jenkins user-password credential.
+
+*/
+
 import jenkins.model.*
 
-def mailConfigurationUsername = "outbound-mail-configuration"
+println "------ Set Jenkins outbound e-mail configuration -------------------"
+
+// Check the presence of the MAIL-USER credential set
+def mailUser    = "MAIL-USER"
 def credentials = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
                        com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials.class,
                        jenkins.model.Jenkins.instance )
+def mailCreds   = credentials.findResult { it.username == mailUser ? it : null }
+if ( mailCreds == null )
+{
+    println ""
+    println "ERROR: Check the presence of the MAIL-USER credential set."
+    println ""
+}
 
-def mailCredentials = credentials.findResult { it.username == mailConfigurationUsername ? it : null }
-
-if ( ! mailCredentials ) {
-    println "Jenkins credential NOT found for ${mailConfigurationUsername}"
-} else {
-    println "Jenkins credential ${mailCredentials.id} found for username ${mailCredentials.username}"
-    
-    // Password contains:
-    // <host>:<port>:<ssl?>:<account>:<password>:<replytoaddress>:<charset>
-    // E.g: mail.hdml.nl:25:true:hans@hdml.nl:<secret>:noreply@hdml.nl:UTF-8
-    def String password = mailCredentials.password
-    def String[] omc    = password.split(":")
+// Check the presence of all JENKINS_MAIL environment variables
+def Map ev = System.getenv()
+if ( ev["JENKINS_MAIL_HOST"]        == null || ev["JENKINS_MAIL_PORT"]     == null || 
+     ev["JENKINS_MAIL_SSL"]         == null || ev["JENKINS_MAIL_USER"]     == null ||
+     ev["JENKINS_REPLY_TO_ADDRESS"] == null || ev["JENKINS_MAIL_CHARSET"]  == null )
+{
+    println ""
+    println "ERROR: Check the presence of all JENKINS_MAIL_XXX environment variables."
+    println "       One of more variables are missing.
+    println ""
   
-    println "Mail host    = ${omc[0]}"
-    println "Mail port    = ${omc[1]}"
-    println "SSL ?        = ${omc[2]}"
-    println "Mail account = ${omc[3]}"
-    println "Mail passwd  = <secret>"
-    println "Mail replyTo = ${omc[5]}"
+    ev = null
+}
+
+if ( mailCreds == null || ev == null ) 
+{
+    println "ERROR: Jenkins Outbound Mail configuration NOT set."
+    return
+}
+else
+{
+    println "Jenkins Mail host    = ${ev["JENKINS_MAIL_HOST"]}"
+    println "Jenkins Mail port    = ${ev["JENKINS_MAIL_PORT"]}"
+    println "Jenkins Mail SSL ?   = ${ev["JENKINS_MAIL_SSL"]}"
+    println "Jenkins Mail user    = ${ev["JENKINS_MAIL_USER"]}"
+    println "Jenkins Mail passwd  = <secret>"
+    println "Jenkins Mail replyTo = ${ev["JENKINS_REPLY_TO_ADDRESS"]
+}"
     println "Mail charset = ${omc[6]}"
 
     def mailDesc = jenkins.model.Jenkins.instance.getDescriptor("hudson.tasks.Mailer")
