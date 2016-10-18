ad1735b49d88d2a0d8bccbf375fced753047f8b9
kosteh <kosteh@users.noreply.github.com>
2016-10-18 08:17:32 +0200

    Update 120-configure-maven-installers.groovy

--- docker-image/ref/init.groovy.d/120-configure-maven-installers.groovy
+++ docker-image/ref/init.groovy.d/120-configure-maven-installers.groovy
@@ -3,3 +3,27 @@ println ""
 println "INFO : No Maven installers defined."
 println ""
 println "------ END ---------------------------------------------------------"
+
+return
+
+import jenkins.model.*
+
+println "Adding an auto installers for Maven"
+
+def mavenPluginExtension = Jenkins.instance.getExtensionList(hudson.tasks.Maven.DescriptorImpl.class)[0]
+
+// Current installations:
+// def asList = (mavenPluginExtension.installations as List)
+// Start over with empty list of installations
+aslist = []
+// Add installations 
+asList.add(new hudson.tasks.Maven.MavenInstallation('Maven Latest', null, 
+      [new hudson.tools.InstallSourceProperty([new hudson.tasks.Maven.MavenInstaller("3.3.9")])]
+asList.add(new hudson.tasks.Maven.MavenInstallation('Maven 3.3.9', null, 
+      [new hudson.tools.InstallSourceProperty([new hudson.tasks.Maven.MavenInstaller("3.3.9")])]
+
+mavenPluginExtension.installations = asList
+
+mavenPluginExtension.save()
+println "OK - Maven auto-installers (from Apache) added" 
+
