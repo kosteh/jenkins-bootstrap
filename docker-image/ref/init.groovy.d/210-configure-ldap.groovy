dc88a8603c46add79de683f1260cef799e9ae5f6
kosteh <kosteh@users.noreply.github.com>
2016-10-19 23:14:30 +0200

    Create 210-configure-ldap.groovy

---
+++ docker-image/ref/init.groovy.d/210-configure-ldap.groovy
@@ -0,0 +1,57 @@
+/*
+
+Purpose:
+This script configures the Jenkins LDAP configuration, that can be specified manually through the
+configuration page "Jenkins->Manage Jenkins->Configure Global Security".
+
+Usage:
+The configuration parameters are provided through environment variables named JENKINS_LDAP_XXX mainly. 
+The password is provided by a Jenkins username/password credential named ???????.
+
+*/
+
+import jenkins.model.*
+import hudson.security.*
+import org.jenkinsci.plugins.*
+
+println "------ Jenkins LDAP configuration ----------------------------------"
+println ""
+
+def ev = System.getenv()
+
+def server                     = ev["JENKINS_LDAP_SERVER"] ? ev["JENKINS_LDAP_SERVER"] : 'kadaster.local:3268'
+def rootDN                     = ev["JENKINS_LDAP_ROOTDN"] ? ev["JENKINS_LDAP_ROOTDN"] : 'dc=kadaster,dc=local'
+def userSearchBase             = ev["JENKINS_LDAP_USER_SEARCHBASE"] ? ev["JENKINS_LDAP_USER_SEARCHBASE"] : ''
+def userSearch                 = ev["JENKINS_LDAP_USER_SEARCH"] ? ev["JENKINS_LDAP_USER"] : ''
+def groupSearchBase            = ev["JENKINS_LDAP_GROUP_SEARCHBASE"] ? ev["JENKINS_LDAP_GROUP_SEARCHBASE"] : ''
+def managerDN                  = ev["JENKINS_LDAP_MANAGERDN"] ? ev["JENKINS_LDAP_MANAGERDN"] : ldapCreds.user
+def managerPassword            = ev["JENKINS_LDAP_PASSWORD"] ? ev["JENKINS_LDAP_PASSWORD"] : ldapCreds.password.toString()
+def inhibitInferRootDN         = ev["JENKINS_LDAP_INHIBIT_INFER_ROOTDN"] ? Boolean.valueOf(ev["JENKINS_LDAP_INHIBIT_INFER_ROOTDN"]) : false
+def disableMailAddressResolver = ev["JENKINS_LDAP_DISABLE_MAIL_ADDRESS_RESOLVER"] ? Boolean.valueOf(ev["JENKINS_LDAP_DISABLE_MAIL_ADDRESS_RESOLVER"]) : false
+
+println "Server : ${server}"
+println "RootDN : ${rootDN}"
+println "UserSearchBase : ${userSearchBase}"
+println "UserSearch : ${userSearch}"
+println "GroupSearchBase : ${groupSearchBase}"
+println "ManagerDN : ${managerDN}"
+println "ManagerPassword : <secret>"
+println "InhibitInferRootDN : ${inhibitInferRootDN.toString()}"
+println "DisableMailAddressResolver : ${disableMailAddressResolver.toString()}"
+
+SecurityRealm ldapRealm = new LDAPSecurityRealm(server, rootDN, userSearchBase, userSearch, 
+                                                groupSearchBase, managerDN, managerPassword, inhibitInferRootDN)
+if (ldapRealm)
+{
+    Jenkins.instance.setSecurityRealm(ldapRealm)
+    Jenkins.instance.save()
+    
+    println "INFO : LDAP configured successfully"
+} 
+else
+{
+    println "ERROR: LDAP configuration failed"
+}
+
+println ""
+println "------ END ---------------------------------------------------------"
