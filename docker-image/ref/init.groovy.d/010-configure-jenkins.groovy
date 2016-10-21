38840dd8ea1160178e8595e0d7affc3e05c49f0e
kosteh <kosteh@users.noreply.github.com>
2016-10-21 23:20:25 +0200

    Update 010-configure-jenkins.groovy

--- docker-image/ref/init.groovy.d/010-configure-jenkins.groovy
+++ docker-image/ref/init.groovy.d/010-configure-jenkins.groovy
@@ -5,7 +5,7 @@ This script configures the Jenkins main properties, that can be specified manual
 configuration page "Jenkins->Manage Jenkins->Configure System", first "unnamed" section.
 
 Usage:
-The configuration parameters are provided through environment variables named JENKINS_XXX. 
+The configuration parameters are provided through environment variables named JENKINS_MAIN_XXX. 
 Otherwise, sensible defaults are used.
 
 */
@@ -17,13 +17,25 @@ def jenkins = Jenkins.getInstance()
 println "------ Setting Jenkins main properties -----------------------------"
 println ""
 
+// Set workspace directory (to high performance storage) and the builds directory
 //jenkins.rawWorkspaceDir = '/tmp/workspaces/' + '${ITEM_FULL_NAME}'
 jenkins.rawBuildsDir    = '/tmp/builds/' + '${ITEM_FULL_NAME}'
 
 println "${jenkins.rawWorkspaceDir}"
 println "${jenkins.rawBuildsDir}"
 
-// 
+// Set 
+jenkins.setSystemMessage('<h1>SYSTEM MESSAGE</h1>')
+
+// Set humber of executors
+jenkins.setNumExecutors(4)
+
+// Set master execution labels
+jenkins.setLabelString('master')
+
+// Persist the Jenkins configuration
+jenkins.save()
+
 
 println ""
 println "------ END ---------------------------------------------------------"
