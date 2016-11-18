2456e9d6ed3701a7c77a62e8aea0a33423d7a83c
kosteh <kosteh@users.noreply.github.com>
2016-11-18 22:35:35 +0100

    Update 005-set-default-jenkins-properties.groovy

--- docker-image/ref/init.groovy.d/005-set-default-jenkins-properties.groovy
+++ docker-image/ref/init.groovy.d/005-set-default-jenkins-properties.groovy
@@ -1,1 +1,7 @@
-// Sets default Jenkins properties
+// Sets the default Jenkins properties
+
+Properties props = new Properties()
+File propsFile = new File('/tmp/jenkins.properties')
+props.load(propsFile.newDataInputStream())
+
+for (prop in props) println "$prop.key = $prop.value"
