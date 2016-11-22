75f7b3c40976455a9f5b28934dcf3fdc19744484
kosteh <kosteh@users.noreply.github.com>
2016-11-22 09:48:21 +0100

    Update 130-configure-docker-installers.groovy

--- docker-image/ref/init.groovy.d/130-configure-docker-installers.groovy
+++ docker-image/ref/init.groovy.d/130-configure-docker-installers.groovy
@@ -8,27 +8,35 @@ println ""
 
 def descriptor = Jenkins.getInstance().getDescriptor("org.jenkinsci.plugins.docker.commons.tools.DockerTool")
 
-// Define and configure a number of JDK installations
-def versions = [
+// Define and configure a number of DockerTool installations
+def dockertools = [
   "Docker Latest" : "1.12.3",
   "Docker 1.12"   : "1.12.3",
   "Docker 1.12.3" : "1.12.3",
   "Docker 1.11"   : "1.11.2",
-  "Docker 1.12.3" : "1.11.2"
+  "Docker 1.11.2" : "1.11.2"
 ]
 
+// List of Dockertool installations
+def installations = []
+
+// Remove existing Dockertool installations
 if (descriptor.getInstallations()) {
     println 'Docker Tools already configured'
-} else {
-    for (v in versions) {
-        println "${v.key}: ${v.value}"
-        def dockerInstaller = new DockerToolInstaller(v.key, v.value)
-        def installerProps = new InstallSourceProperty([dockerInstaller])
-        def installation = DockerTool(v.key, null, [installerProps])
-        descriptor.setInstallations(docker)
+}
+
+// Configure Dockertool configurations
+for (tool in dockertools) {
+        println "${tool.key} : ${tool.value}"
+        def installer = new DockerToolInstaller(tool.key, tool.value)
+        def installSourceProp = new InstallSourceProperty([installer])
+        def installation = new DockerTool(v.key, null, [installSourceProp])
+        installations.push(installation)
     }
-}   
-print descriptor.getInstallations()
+    descriptor.setInstallations(installations.toArray(new DockerTool[0]))
+    descriptor.save()
+//}
+descriptor.getInstallations().each { println it }
 
 println ""
 println "------ END ---------------------------------------------------------"
