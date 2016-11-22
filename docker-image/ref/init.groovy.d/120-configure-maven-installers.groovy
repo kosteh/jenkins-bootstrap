1ccc338a2409714aa5e5ad2a34878107e3258d4f
kosteh <kosteh@users.noreply.github.com>
2016-11-22 18:53:58 +0100

    Update 120-configure-maven-installers.groovy

--- docker-image/ref/init.groovy.d/120-configure-maven-installers.groovy
+++ docker-image/ref/init.groovy.d/120-configure-maven-installers.groovy
@@ -1,24 +1,20 @@
 /*
 
 Purpose:
-Configures a number of Maven auto-installers.
-
-Usage:
-Define one or more versions in the "versions" map.
+Configures a number of JDK auto-installers in a Jenkins instance.
 
 */
 
-import jenkins.model.*
-import hudson.tasks.*
-import hudson.tools.*
-  
-println "------ Configure Maven installers ------------------------------------"
+import hudson.tasks.Maven
+import hudson.tools.MavenInstaller
+import hudson.tools.InstallSourceProperty
+import jenkins.model.Jenkins
+
+println "------ Configure Maven ------------------------------------------------"
 println ""
 
-def desc = Jenkins.instance.getDescriptor("hudson.tasks.Maven")
-
-// Define and configure a number of MAven installations
-def versions = [
+// Define and configure a number of Maven installations
+def tools = [
   "Maven Latest"   : "3.3.9",
   "Maven 3 Latest" : "3.3.9",
   "Maven 3.3.9"    : "3.3.9",
@@ -26,19 +22,33 @@ def versions = [
   "Maven 2.2.1"    : "2.2.1"
 ]
 
-def installations = [];
+// Remove existing Maven installations from the "tools" directory
+// TODO: find out how to remove the directory on a Jenkins agent.
+def dir = "${System.getenv("JENKINS_HOME")}/tools/hudson.tasks.Maven"
+def toolDir = new File(dir)
+if ( toolDir.deleteDir()) {
+  println "Tool directory ${dir} deleted successfully\n"
+}
 
-for (v in versions) {
-  println "${v.key}: ${v.value}"
-  def installer = new Maven.MavenInstaller(v.value)
-  def installerProps = new InstallSourceProperty([installer])
-  def installation = new Maven.MavenInstallation(v.key, null, [installerProps])
+// Configure Maven
+def installations = []
+def descriptor = Jenkins.getInstance().getDescriptor("hudson.tasks.Maven")
+
+for (tool in tools) {
+  def installer = new MavenInstaller(tool.value, true)
+  def installSourceProp = new InstallSourceProperty([installer])
+  def installation = new MavenInstallation(tool.key, null, [installSourceProp])
   installations.push(installation)
 }
 
-// Persist the Maven tool configuration
-desc.setInstallations(installations.toArray(new Maven.MavenInstallation[0]))
-desc.save() 
+// Persist the Maven configuration
+descriptor.setInstallations(installations.toArray(new MavenInstallation[0]))
+descriptor.save()
+
+// Print configured Maven versions
+descriptor.getInstallations().each { MavenInstallation tool -> println "${tool.getName()} : ${tools[tool.getName()]}" }
 
 println ""
 println "------ END ---------------------------------------------------------"
+
+
