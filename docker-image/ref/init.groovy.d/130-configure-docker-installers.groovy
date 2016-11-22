a2221a728716c4ea1438dbd776adfb98ca13881a
kosteh <kosteh@users.noreply.github.com>
2016-11-22 07:51:35 +0100

    Update 130-configure-docker-installers.groovy

--- docker-image/ref/init.groovy.d/130-configure-docker-installers.groovy
+++ docker-image/ref/init.groovy.d/130-configure-docker-installers.groovy
@@ -1,5 +1,21 @@
-println "------ Configure Docker installers ---------------------------------"
+import org.jenkinsci.plugins.docker.commons.tools.DockerTool
+import org.jenkinsci.plugins.docker.commons.tools.DockerToolInstaller
+import hudson.tools.InstallSourceProperty
+import jenkins.model.Jenkins
+
+println "------ Configure Docker client ---------------------------------"
 println ""
-println "INFO : No Docker installers defined."
+
+def descriptor = new DockerTool.DescriptorImpl();
+
+if (descriptor.getInstallations()) {
+    println 'skip Docker Tool installations'
+} else {
+    //Jenkins.instance.updateCenter.getById('default').updateDirectlyNow(true)
+    def dockerInstaller = new DockerToolInstaller('Docker1.12.3', "1.12.3")
+    def docker = new DockerTool("Docker-1.12", null, [new InstallSourceProperty([dockerInstaller])])
+    descriptor.setInstallations(docker)
+}
+
 println ""
 println "------ END ---------------------------------------------------------"
