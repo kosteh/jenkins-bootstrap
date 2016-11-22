44ee76a2d4a0b10ac806ea662e2579e1b9341c56
kosteh <kosteh@users.noreply.github.com>
2016-11-22 19:31:33 +0100

    Update 110-configure-nodejs-installers.groovy

--- docker-image/ref/init.groovy.d/110-configure-nodejs-installers.groovy
+++ docker-image/ref/init.groovy.d/110-configure-nodejs-installers.groovy
@@ -1,4 +1,59 @@
 /*
+Purpose:
+Configures a number of NodeJS auto-installers in a Jenkins instance.
+*/
+
+import jenkins.plugins.nodejs.tools.NodeJSInstaller
+import jenkins.plugins.nodejs.tools.NodeJSInstallation
+import hudson.tools.InstallSourceProperty
+import jenkins.model.Jenkins
+
+println "------ Configure NodeJS installations ----------------------------------------"
+println ""
+
+// Define a number of NodeJS installations
+def tools = [
+  "NodeJS Latest": "NodeJS 6.7.0",
+  "NodeJS 6.7.0" : "NodeJS 6.7.0",
+  "NodeJS 6"     : "NodeJS 6.2.0",
+  "NodeJS 4"     : "NodeJS 4.1.2",
+  "NodeJS 4 (Windows)" : "NodeJS 4.4.4"
+]
+
+// Remove existing NodeJS installations from the "tools" directory.
+// TODO: find out how to remove the directory on a Jenkins agent.
+def dir = "${System.getenv("JENKINS_HOME")}/tools/jenkins.plugins.nodejs.tools.NodeJSInstallation"
+def toolDir = new File(dir)
+if ( toolDir.deleteDir()) {
+  println "Tool directory ${dir} deleted successfully\n"
+}
+
+// Configure the NodeJS installations
+def installations = []
+def descriptor = Jenkins.getInstance().getDescriptor("jenkins.plugins.nodejs.tools.NodeJSInstallation")
+
+for (tool in tools) {
+  def installer = new NodeJSInstaller(v.value, "bower bower-art", 100)
+  def installSourceProp = new InstallSourceProperty([installer])
+  def installation = new NodeJSInstallation(tool.key, null, [installSourceProp])
+  installations.push(installation)
+}
+
+// Persist the NodeJS configuration
+descriptor.setInstallations(installations.toArray(new JDK[0]))
+descriptor.save()
+
+// Print installed NodeJS versions
+descriptor.getInstallations().each { NodeJSInstallation tool -> println "${tool.getName()} : ${tools[tool.getName()]}" }
+
+println ""
+println "------ END ---------------------------------------------------------"
+
+
+
+
+
+/*
 
 Purpose:
 Configures a number of NodeJS auto-installers.
@@ -23,6 +78,9 @@ def desc = inst.getDescriptor("jenkins.plugins.nodejs.tools.NodeJSInstallation")
 def versions = [
   "NodeJS Latest": "NodeJS 6.7.0",
   "NodeJS 6.7.0" : "NodeJS 6.7.0",
+  "NodeJS 6"     : "NodeJS 6.2.0",
+  "NodeJS 4"     : "NodeJS 4.1.2",
+  "NodeJS 4 (Windows)" : "NodeJS 4.4.4"
 ]
 
 def installations = [];
