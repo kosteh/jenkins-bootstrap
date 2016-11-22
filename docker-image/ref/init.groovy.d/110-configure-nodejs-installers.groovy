aff902c6ac231b58482dc8ca94184708bfab3bd5
kosteh <kosteh@users.noreply.github.com>
2016-11-22 21:06:27 +0100

    Update 110-configure-nodejs-installers.groovy

--- docker-image/ref/init.groovy.d/110-configure-nodejs-installers.groovy
+++ docker-image/ref/init.groovy.d/110-configure-nodejs-installers.groovy
@@ -13,10 +13,10 @@ println ""
 
 // Define a number of NodeJS installations
 def tools = [
-  "NodeJS Latest": "NodeJS 6.7.0",
-  "NodeJS 6.7.0" : "NodeJS 6.7.0",
-  "NodeJS 6"     : "NodeJS 6.2.0",
-  "NodeJS 4"     : "NodeJS 4.1.2",
+  "NodeJS Latest"      : "NodeJS 6.7.0",
+  "NodeJS 6.7.0"       : "NodeJS 6.7.0",
+  "NodeJS 6"           : "NodeJS 6.2.0",
+  "NodeJS 4"           : "NodeJS 4.1.2",
   "NodeJS 4 (Windows)" : "NodeJS 4.4.4"
 ]
 
@@ -33,69 +33,18 @@ def installations = []
 def descriptor = Jenkins.getInstance().getDescriptor("jenkins.plugins.nodejs.tools.NodeJSInstallation")
 
 for (tool in tools) {
-  def installer = new NodeJSInstaller(v.value, "bower bower-art", 100)
+  def installer = new NodeJSInstaller(tool.value, "bower bower-art", 100)
   def installSourceProp = new InstallSourceProperty([installer])
   def installation = new NodeJSInstallation(tool.key, null, [installSourceProp])
   installations.push(installation)
 }
 
 // Persist the NodeJS configuration
-descriptor.setInstallations(installations.toArray(new JDK[0]))
+descriptor.setInstallations(installations.toArray(new NodeJSInstallation[0]))
 descriptor.save()
 
 // Print installed NodeJS versions
 descriptor.getInstallations().each { NodeJSInstallation tool -> println "${tool.getName()} : ${tools[tool.getName()]}" }
-
-println ""
-println "------ END ---------------------------------------------------------"
-
-
-
-
-
-/*
-
-Purpose:
-Configures a number of NodeJS auto-installers.
-
-Usage:
-Define one or more versions in the "versions" map.
-
-*/
-
-import jenkins.model.*
-import hudson.model.*
-import jenkins.plugins.nodejs.tools.*
-import hudson.tools.*
-
-println "------ Configure NodeJS installers ---------------------------------"
-println ""
-
-def inst = Jenkins.getInstance()
-def desc = inst.getDescriptor("jenkins.plugins.nodejs.tools.NodeJSInstallation")
-
-// Define and configure a number of NodeJS installations
-def versions = [
-  "NodeJS Latest": "NodeJS 6.7.0",
-  "NodeJS 6.7.0" : "NodeJS 6.7.0",
-  "NodeJS 6"     : "NodeJS 6.2.0",
-  "NodeJS 4"     : "NodeJS 4.1.2",
-  "NodeJS 4 (Windows)" : "NodeJS 4.4.4"
-]
-
-def installations = [];
-
-for (v in versions) {
-  println "${v.key}: ${v.value}"
-  def installer = new NodeJSInstaller(v.value, "bower bower-art", 100)
-  def installerProps = new InstallSourceProperty([installer])
-  def installation = new NodeJSInstallation(v.key, "", [installerProps])
-  installations.push(installation)
-}
-
-// Persist the NodeJS configuration
-desc.setInstallations(installations.toArray(new NodeJSInstallation[0]))
-desc.save()  
 
 println ""
 println "------ END ---------------------------------------------------------"
